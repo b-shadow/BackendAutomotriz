170d7b882756d4f7975b92c046b58cd3e9e3ad55
b-shadow <brandonlidervasquezardaya@gmail.com>
2026-05-10 01:21:11 -0400

    corrigiendo rutas de backups

--- modulos/comunicacion_control_inteligencia/serializers/backups.py
+++ modulos/comunicacion_control_inteligencia/serializers/backups.py
@@ -1,4 +1,6 @@
-﻿from rest_framework import serializers
+import datetime
+
+from rest_framework import serializers
 
 from modulos.comunicacion_control_inteligencia.models import (
     BackupEmpresa,
@@ -27,6 +29,14 @@ class BackupEmpresaSerializer(serializers.ModelSerializer):
 
 
 class ProgramacionBackupSerializer(serializers.ModelSerializer):
+    class SafeTimeField(serializers.TimeField):
+        def to_representation(self, value):
+            if isinstance(value, datetime.datetime):
+                value = value.time()
+            return super().to_representation(value)
+
+    hora_ejecucion = SafeTimeField()
+
     class Meta:
         model = ProgramacionBackupEmpresa
         fields = [
--- modulos/comunicacion_control_inteligencia/urls.py
+++ modulos/comunicacion_control_inteligencia/urls.py
@@ -5,9 +5,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from modulos.comunicacion_control_inteligencia.viewsets.ia_viewsets import IAViewSet
+from modulos.comunicacion_control_inteligencia.viewsets.backups import BackupEmpresaViewSet
 
 router = DefaultRouter()
 router.register(r'ia', IAViewSet, basename='ia-assistant')
+router.register(r'backups', BackupEmpresaViewSet, basename='backups-empresa')
 
 urlpatterns = [
     path('', include(router.urls)),
